d3cf3b344f4556cbe0182884eb7adc7bfdd2e727
Nikhil Wale <walenikhil2@gmail.com>
2020-10-11 03:15:43 +0530

    this is commit made after adding comments into the java code

--- First.java
+++ First.java
@@ -1,5 +1,5 @@
-class First{
-public static void main(String args[])
+class First{                            // this is starting of java class
+public static void main(String args[])  // this is main function
 {
 System.out.println("Hello Java");
 }
